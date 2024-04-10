d146182639cf64e79510474192e2c56a97a6000b
ssadown <bibatututu@yandex.ru>
2024-04-10 15:37:33 +0500

    Сделал блок для компетенций

---
+++ src/Components/Competence .jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Competence = ({
+    isICan,
+    nameOfCompetence,
+    informationAboutCompetence,
+    levelOfCompetence
+}) => {
+    return (
+        <>
+            <div className={isICan ? 'competence-block can' : 'competence-block cant'}>
+                <h2>{nameOfCompetence}</h2>
+                <p className='competence-info'>{informationAboutCompetence}</p>
+                {isICan && (
+                    <p className='competence-level'>Я оцениваю свою уровень на <span>{levelOfCompetence}</span>/5</p>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default Competence;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Me from '../assets/Me.png'
 import AboutMeBlock from './AboutMeBlock';
+import Competence from './Competence ';
 
 const Main = () => {
-    const infoAboutPerson = {
+    const [infoAboutPerson, setInfoAboutPerson] = useState({
             imageOfPerson: Me,
             nameOfPerson: 'Милюков Леонид Олегович'
+        })
+    const [myCompetence, setMyCompetence] = useState([
+        {
+            isICan: true,
+            nameOfCompetence: 'React',
+            informationAboutCompetence: 'А',
+            levelOfCompetence: '3.5'
         }
+    ])
     return (
         <>
             <main>
@@ -14,6 +23,16 @@ const Main = () => {
                     imageOfPerson={infoAboutPerson.imageOfPerson}
                     nameOfPerson={infoAboutPerson.nameOfPerson}
                 />
+                {myCompetence.map(competence => {
+                    return (
+                        <Competence
+                            isICan={competence.isICan}
+                            nameOfCompetence={competence.nameOfCompetence}
+                            informationAboutCompetence={competence.informationAboutCompetence}
+                            levelOfCompetence={competence.levelOfCompetence}
+                        />
+                    )
+                })}
             </main>
         </>
     );
