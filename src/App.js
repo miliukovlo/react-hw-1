dd46e3567f7d52de8bd11238f05a76e55f39751c
ssadown <bibatututu@yandex.ru>
2024-04-13 13:53:17 +0500

    Сделал роутер для приложения

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './styles/App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginPage from './Pages/LoginPage';
-import { userContext } from './Context/Context';
+import { loginContext, userContext } from './Context/Context';
+import MainPage from './Pages/MainPage';
 
 const App = () => {
   const [nameOfUser, setNameOfUser] = useState('')
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('login')) {
+      setIsLogin(true)
+    }
+    if (localStorage.getItem('nameOfUser')) {
+      setNameOfUser(JSON.parse(localStorage.getItem('login')))
+    }
+  },[])
 
   return (
+    isLogin ? 
+    <loginContext.Provider value={{isLogin, setIsLogin}}>
     <userContext.Provider value={{nameOfUser, setNameOfUser}}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LoginPage/>}/>
+          <Route path='/competent' element={<MainPage/>} exact/>
+          <Route path='*' element={<Navigate to='/competent' replace/>} exact/>
         </Routes>
       </BrowserRouter>
     </userContext.Provider>
+  </loginContext.Provider>
+  :
+  <loginContext.Provider value={{isLogin, setIsLogin}}>
+  <userContext.Provider value={{nameOfUser, setNameOfUser}}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage/>} exact/>
+        <Route path='*' element={<Navigate to='/' replace/>} exact/>
+      </Routes>
+    </BrowserRouter>
+  </userContext.Provider>
+</loginContext.Provider>
   );
 }
 
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <>
+            <main>
+                <h1>HELLO WORLD!</h1>
+            </main>
+        </>
+    );
+}
+
+export default MainPage;
