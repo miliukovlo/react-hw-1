86cc685bd63d23e44dd2a8875244e01ece4bd0e1
ssadown <bibatututu@yandex.ru>
2024-04-16 14:29:56 +0500

    Выполнил 1 задание по созданию таймера

---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CountdownTimer from './CountdownTimer';
+
+const Main = () => {
+    return (
+        <>
+            <main>
+                <CountdownTimer/>
+            </main>
+        </>
+    );
+}
+
+export default Main;
