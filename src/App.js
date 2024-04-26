9ee3b430d8f14b7ed9d05d5a4dad195d79da2f83
ssadown <bibatututu@yandex.ru>
2024-04-26 19:53:03 +0500

    Очистил проект

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Main from './Components/Main';
 import './styles/App.css';
 
 import React from 'react';
@@ -8,10 +5,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <Header/>
-      {/* Компонент Main создан для помещения туда основного контента */}
-      <Main/>
-      <Footer/>
+      <h1>Проверка запуска</h1>
     </>
   );
 }
