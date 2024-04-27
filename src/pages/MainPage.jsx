913d4ee72cd7f1a32ae2728d1f36ff42c97fdc26
ssadown <bibatututu@yandex.ru>
2024-04-27 13:24:16 +0500

    Добавил API

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MainPage = () => {
+
+    useEffect(() => {
+        fetch('https://api.kinopoisk.dev/v1.4/movie?page=1&limit=50', {
+            headers: {
+                'X-API-KEY': '2CNPDYY-EB2MANC-QHGWFNW-WVCT91P'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Не получилось получить данные!');
+            }
+            return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+        
+    }, [])
+
     return (
         <div>
             <h1>Hello</h1>
