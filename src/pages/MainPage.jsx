d243f37b066b27bf946c64e5f1f42fe4257f48af
ssadown <bibatututu@yandex.ru>
2024-04-29 20:29:54 +0500

    Добавил observer для автозагрузки фильмов

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useGetFilms } from '../hooks/useGetFilms';
 import './styles/MainPageStyle.css'
 import FilmCard from '../Components/MainPage/FilmCard/FilmCard';
@@ -10,7 +10,25 @@ const MainPage = () => {
     const observer = useRef(null)
     const contentRef = useRef(null)
 
-    console.log(popularFilms)
+    useEffect(() => {
+        observer.current = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setLimit(prevLimit => prevLimit + 10);
+                }
+            });
+        });
+
+        if (contentRef.current) {
+            observer.current.observe(contentRef.current);
+        }
+
+        return () => {
+            if (observer.current) {
+                observer.current.disconnect();
+            }
+        };
+    }, [limit])
 
     return (
         <>
@@ -20,7 +38,7 @@ const MainPage = () => {
                 <FilmCard key={id} id={id} poster={poster.url} title={name} genres={genres} />
             ))}
             </main>
-            <div className="main-page__observer"></div>
+            <div ref={contentRef} className="main-page__observer"></div>
         </>
     );
 }
